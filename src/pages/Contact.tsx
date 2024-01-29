c9d4fd16d95a975a479ee9a4f6d81f08417da57c
aw1xess <yariksvirshchuk@gmail.com>
2024-01-29 11:54:37 +0200

    React import removed where unnecessary

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled, { keyframes } from "styled-components";
 import email from "../assets/icons/email.png";
 import telegram from "../assets/icons/telegram.png";
