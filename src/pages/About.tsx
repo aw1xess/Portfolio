7b8826ba653c62d2cb7e81531ae8d89a7eb9a5b6
aw1xess <yariksvirshchuk@gmail.com>
2024-10-17 15:09:13 +0300

    Animations removed in project section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,110 +10,111 @@ const fadeIn = keyframes`
 `;
 
 const Container = styled.div`
-	width: 100%;
-	display: grid;
-	justify-items: end;
-	align-items: end;
-	animation: 1s ${fadeIn} ease-out;
+    width: 100%;
+    display: grid;
+    justify-items: end;
+    align-items: end;
+    animation: 1s ${fadeIn} ease-out;
 `;
 
 const Content = styled.section`
-	width: 18vw;
-	display: grid;
-	gap: 3em;
-	text-align: end;
-	margin-bottom: 10px;
-	& p {
-		font-size: 0.8em;
-		opacity: 0.85;
-	}
+    width: 18vw;
+    display: grid;
+    gap: 3em;
+    text-align: end;
+    margin-bottom: 10px;
+    & p {
+        font-size: 0.8em;
+        opacity: 0.85;
+    }
 
-	@media screen and (max-width: 1200px) {
-		width: 20vw;
-	}
+    @media screen and (max-width: 1200px) {
+        width: 20vw;
+    }
 
-	@media screen and (max-width: 850px) {
-		width: 24vw;
+    @media screen and (max-width: 850px) {
+        width: 24vw;
 
-		& p {
-			font-size: 0.9em;
-		}
-	}
+        & p {
+            font-size: 0.9em;
+        }
+    }
 
-	@media screen and (max-width: 600px) {
-		width: 38vw;
-	}
+    @media screen and (max-width: 600px) {
+        width: 38vw;
+    }
 
-	@media screen and (max-width: 525px) {
-		width: 38vw;
-	}
+    @media screen and (max-width: 525px) {
+        width: 38vw;
+    }
 `;
 
 const Header = styled.h3`
-	font-size: 1.2em;
-	margin-bottom: 16px;
-	font-weight: 500;
-	border-bottom: 1px solid #fff;
+    font-size: 1.2em;
+    margin-bottom: 16px;
+    font-weight: 500;
+    border-bottom: 1px solid #fff;
 `;
 
 const SubHeader = styled.h4`
-	font-size: 0.9em;
-	font-weight: 500;
+    font-size: 0.9em;
+    font-weight: 500;
 
-	@media screen and (max-width: 850px) {
-		font-size: 1em;
-	}
+    @media screen and (max-width: 850px) {
+        font-size: 1em;
+    }
 `;
 
 const ToolsItem = styled.li`
-	font-size: 0.8em;
-	opacity: 0.85;
-	&:not(:last-child) {
-		margin-bottom: 0.6em;
-	}
+    font-size: 0.8em;
+    opacity: 0.85;
+    &:not(:last-child) {
+        margin-bottom: 0.6em;
+    }
 
-	@media screen and (max-width: 850px) {
-		font-size: 0.9em;
-	}
+    @media screen and (max-width: 850px) {
+        font-size: 0.9em;
+    }
 `;
 
 const About = () => {
-	return (
-		<Container>
-			<Content>
-				<div>
-					<Header>About me</Header>
-					<p>
-						I'm 20 years old Front-End developer from Ukraine. I
-						like coding websites, playing videogames with great
-						story, city walks with friends or by myself and
-						listening to good music.
-					</p>
-				</div>
-				<div>
-					<Header>What tools do I use?</Header>
-					<ul>
-						<li>
-							<SubHeader>Structure and Logic</SubHeader>
-						</li>
-						<ToolsItem>
-							React + TypeScript, Redux Toolkit, React Router
-						</ToolsItem>
-						<li>
-							<SubHeader>Styles</SubHeader>
-						</li>
-						<ToolsItem>
-							Styled Components, Sass, Tailwind, Bootstrap
-						</ToolsItem>
-						<li>
-							<SubHeader>Libraries and Frameworks</SubHeader>
-						</li>
-						<ToolsItem>Swiper, ReactDnD, SlickJs etc.</ToolsItem>
-					</ul>
-				</div>
-			</Content>
-		</Container>
-	);
+    return (
+        <Container>
+            <Content>
+                <div>
+                    <Header>About me</Header>
+                    <p>
+                        Hi! I'm 21 years old Front-End developer from Ukraine. I
+                        want to progress in the field of front-end development
+                        and I am looking for my first job in this specialty. It
+                        is important for me to get the first experience - work
+                        and difficulties do not scare me&#41;
+                    </p>
+                </div>
+                <div>
+                    <Header>What tools do I use?</Header>
+                    <ul>
+                        <li>
+                            <SubHeader>Structure and Logic</SubHeader>
+                        </li>
+                        <ToolsItem>
+                            React + TypeScript, Redux Toolkit, React Router
+                        </ToolsItem>
+                        <li>
+                            <SubHeader>Styles</SubHeader>
+                        </li>
+                        <ToolsItem>
+                            Styled Components, Sass, Scss, Bootstrap
+                        </ToolsItem>
+                        <li>
+                            <SubHeader>Libraries and Frameworks</SubHeader>
+                        </li>
+                        <ToolsItem>Swiper, ReactDnD, SlickJs etc.</ToolsItem>
+                    </ul>
+                </div>
+            </Content>
+        </Container>
+    );
 };
 
 export default About;
